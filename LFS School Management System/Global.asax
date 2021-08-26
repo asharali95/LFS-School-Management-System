15e3c11885009a8303029a8dacd6dd045f539769
Ashar Ali <ashar.alia1999@gmail.com>
2021-08-27 02:40:36 +0500

    Add project files.

---
+++ LFS School Management System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LFS_School_Management_System.MvcApplication" Language="C#" %>
---
+++ LFS School Management System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LFS_School_Management_System.Startup))]
+namespace LFS_School_Management_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
