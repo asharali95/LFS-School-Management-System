c8c5d3abbf10d21077fa2d4fc11105e654cf0a31
Ashar Ali <ashar.alia1999@gmail.com>
2021-09-06 13:36:45 +0500

    lecturer management module added + enrollment schema change

---
+++ LFS School Management System/Models/Meta classes/LecturersMetaData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LFS_School_Management_System.Models
+{
+    public class LecturersMetaData
+    {
+        [Required]
+        [Display(Name ="First Name")]
+        public string First_Name { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Last_Name { get; set; }
+    }
+
+    [MetadataType(typeof(LecturersMetaData))]
+    public partial class Lecturer
+    {
+
+    }
+}
