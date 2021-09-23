fc029271cf309c2c51d0246311c5eb9334dd1ddb
Ashar Ali <ashar.alia1999@gmail.com>
2021-09-23 21:27:15 +0500

    final commit

--- LFS School Management System/Controllers/LecturersController.cs
+++ LFS School Management System/Controllers/LecturersController.cs
@@ -11,7 +11,7 @@ using LFS_School_Management_System.Models;
 
 namespace LFS_School_Management_System.Controllers
 {
-   // [Authorize(Roles ="Admin")]
+   [Authorize(Roles ="Admin")]
     public class LecturersController : Controller
     {
         private LFS_School_ManagementDB_Entities db = new LFS_School_ManagementDB_Entities();
